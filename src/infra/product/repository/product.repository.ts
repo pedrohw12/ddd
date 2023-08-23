18d4847a6043b78b4da90c35ac3b504769ca38d4
Pedro Wanderley <wanderley.pedro@outlook.com>
2023-08-23 16:10:32 -0300

    feat: infra layer module

--- src/infra/product/repository/product.repository.ts
+++ src/infra/product/repository/product.repository.ts
@@ -1,6 +1,6 @@
 import Product from "../../domain/product/entity/product";
 import ProductRepositoryInterface from "../../domain/product/repository/product-repository.interface";
-import ProductModel from "../db/sequelize/model/product.model";
+import ProductModel from "../product/repository/sequelize/product.model";
 
 export default class ProductRepository implements ProductRepositoryInterface {
   async create(entity: Product): Promise<void> {
